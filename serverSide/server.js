e80158a8c640b60ca2954706f163619a14eeb861
Oluwatimileyin Olawuyi <olawuyiolamilekan900@gmail.com>
2022-12-15 08:10:00 +0100

    commit changes

--- serverSide/server.js
+++ serverSide/server.js
@@ -1,77 +1,55 @@
+const Web3 = require("web3");
+const express = require("express");
 
-// const request = require('request');
-const express = require('express');
+// Replace with the actual values of your Ethereum network
+const RPC_URL = "http://localhost:7545";
+const FROM_ADDRESS = "0x...";
+const TO_ADDRESS = "0x...";
+
+// Initialize the web3 instance
+const web3 = new Web3(RPC_URL);
+
+// Initialize the express app
 const app = express();
-const bodyParser = require('body-parser');
- //Use body-parser to parse incoming request bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// List of transactions
-const transactions = [];
+// Create a new transaction
+app.post("/create-transaction", (req, res) => {
+  // Read the values from the request body
+  const { value, gasLimit, gasPrice } = req.body;
 
-// Function to create a new transaction
-function createTransaction(amount, sender, receiver) {
-  // Create a new transaction object with the given values
-  const transaction = {
-    amount: amount,
-    from: sender,
-    to: receiver,
-    status: 'pending', // Set the initial status to pending
+  // Create the raw transaction object
+  const rawTransaction = {
+    from: FROM_ADDRESS,
+    to: TO_ADDRESS,
+    value: web3.utils.toWei(value, "ether"),
+    gasLimit,
+    gasPrice,
   };
 
-  // Add the transaction to the list of transactions
-  transactions.push(transaction);
-
-  // Return the transaction
-  return transaction;
-}
-
-// Function to send a transaction to the frontend for payment
-function sendTransactionForPayment(transaction) {
-  // Send the transaction to the frontend URL for payment
-  request.post('/pay', { json: transaction }, (error, res, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    // Update the transaction status based on the response from the frontend
-    if (res.statusCode === 200) {
-      transaction.status = 'success';
-    } else {
-      transaction.status = 'failed';
-    }
+  // Send the raw transaction to the specified frontend URL
+  fetch(`${req.body.frontendUrl}/send-transaction`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(rawTransaction),
   });
-}
 
-// Function to get the status of a transaction
-function getTransactionStatus(transaction) {
-  return transaction.status;
-}
-
-// Endpoint to create a new transaction
-app.post('/transaction', (req, res) => {
-  // Create a new transaction with the request body
-  const transaction = createTransaction(req.body.amount, req.body.sender, req.body.receiver);
-
-  // Send the transaction to the frontend for payment
-  sendTransactionForPayment(transaction);
-
-  // Return the transaction
-  res.json(transaction);
+  // Return a success message
+  return res.send("Transaction created and sent to frontend");
 });
 
-// Endpoint to get the status of a transaction
-app.get('/transaction/:id', (req, res) => {
-  // Find the transaction with the given ID
-  const transaction = transactions.find(t => t.id === req.params.id);
+// Check the status of a transaction
+app.get("/transaction-status/:transactionHash", (req, res) => {
+  // Get the transaction hash from the URL parameters
+  const { transactionHash } = req.params;
 
-  // Return the status of the transaction
-  res.json({ status: getTransactionStatus(transaction) });
-});
+  try {
+    // Check the transaction receipt to determine the status
+    const receipt = web3.eth.getTransactionReceipt(transactionHash);
 
-// Start the server
-app.listen(5000, () => {
-  console.log('Server listening on port 3000');
+    // Return the transaction receipt
+    return res.send(receipt);
+  } catch (error) {
+    // Return an error if the transaction receipt is not found
+    return res.send(error.message);
+  }
 });
